df8d42e898a94d998be697fd2ce668800fe82cb8
Milan Vasic <vaske494@gmail.com>
2017-05-17 22:26:30 +0200

    Test commit

---
+++ src/BasicFileOps.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BasicFileOps {
+	
+	int finalNumber=0;
+	
+	void mainMethod(final File folder) {
+		
+		for(final File fileEntry : folder.listFiles()) {
+			
+			if(fileEntry.isFile())
+			{
+				int number;
+				int counter = 0;
+				String fileName;
+				
+				fileName = fileEntry.getName();
+				number = Integer.parseInt(fileName.substring(6, 9));
+				try
+				{
+					Scanner scanner = new Scanner(new File(fileEntry.getPath()));
+					while(scanner.hasNextLine())
+					{
+						String line;
+						line = null;
+						line = scanner.nextLine();
+						boolean find = line.contains("PSIML");
+						
+						if(find == true) {
+							counter++;
+						}
+					}
+					if(counter == number)
+					{
+						finalNumber++;
+					}
+					
+					scanner.close();
+				} 
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(fileEntry.isDirectory()) {
+				mainMethod(fileEntry);
+			}
+			else{
+				
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter path to folder:");
+		Scanner in = new Scanner(System.in);
+		String path = in.nextLine();
+		
+		final File myFolder = new File(path);
+		BasicFileOps bfo = new BasicFileOps();
+		bfo.mainMethod(myFolder);
+		System.out.println(bfo.finalNumber);
+		in.close();
+	}
+}
